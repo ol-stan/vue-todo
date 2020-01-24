8f8c4e5050fbde4fcffd4dc656f7a5843ba05340
ol-stan <olstan125@gmail.com>
2020-01-24 22:39:03 +0200

    commit change

--- js/script.js
+++ js/script.js
@@ -1,27 +1,63 @@
 var app = new Vue({
     el: '#app',
     data: {
-        newName: '',
-        list: []
+        newItemName: '',
+        newItemCategory: '',
+        newCategoryName: '',
+        list: [],
+        categories: [{
+                name: 'new'
+            },
+            {
+                name: 'work'
+            },
+            {
+                name: 'home'
+            }
+        ]
     },
     methods: {
         deleteItem(key) {
             this.list.splice(key, 1);
         },
         addItem() {
-            if (this.newName.trim()) {
-                this.list.push({
-                    name: this.newName,
-                    isDone: false,
-                    isEdit: false
-                });
-            }
+            this.list.push({
+                name: this.newItemName,
+                category: this.newItemCategory,
+                isDone: false,
+                isEdit: false
+            });
+            this.newItemName = null; // очещение поля добавления todo
+            this.newItemCategory = '';
+        },
+        addCategory() {
+            this.categories.push({
+                name: this.newCategoryName
+            });
+            this.newCategoryName = null;
+        },
+        filteredList(categoryName) {
+            return this.list.filter(item => item.category === categoryName);
+        }
+    },
+
+    watch: {
+        list: {
+            handler: function () {
+                localStorage.setItem('todoList', JSON.stringify(this.list));
+            },
+            deep: true
+        },
+        categories: function () {
+            localStorage.setItem('todoCategories', JSON.stringify(this.categories));
         }
     },
     created: function () {
         this.list = JSON.parse(localStorage.getItem('todoList')) || [];
+
+        const localStorageCategories = localStorage.getItem('todoCategories');
+        if (localStorageCategories) {
+            this.categories = JSON.parse(localStorage.getItem('todoCategories')) || this.categories;
+        }
     },
-    updated: function () {
-        localStorage.setItem('todoList', JSON.stringify(this.list));
-    }
-})
+});
